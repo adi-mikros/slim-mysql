61fffb5fab149b6beca59882bb3e6112146fe04d
adi-mikros <adi.mikros@gmail.com>
2021-02-06 14:48:07 +0700

    Coba slim mysql

---
+++ README.md
@@ -0,0 +1,1 @@
+# slim-mysql
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+require 'vendor/autoload.php';
+
+$app = new Slim\App;
+
+
+#Container untuk membuat semacam fungsi / function
+$container = $app->getContainer();
+
+
+#middleware untuk CORS
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+#buat container untuk database PDO
+$container['db'] = function(){
+    return new PDO('mysql:local=localhost;dbname=coba-slim', 'root', '');
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//TAMPILKAN DATA contoh
+$app->get('/koperasi', function($request, $response, $datae){
+  $hasil = $this->db->query("select * from koperasi order by cif")->fetchAll(PDO::FETCH_ASSOC);
+  $json=json_encode($hasil);
+	 print_r($json);
+});
+
+
+$app->run();
+
+?>
